db76941d5bf81ca918b146d70f85328527c8e678
Michael Schaecher <MichaelLeeSchaecher@gmail.com>
2025-07-01 21:51:19 -0600

    Switched to direct Debian package build process.

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/grub-theme-polkadots-dark/issues
+# Bug-Submit: https://github.com/<user>/grub-theme-polkadots-dark/issues/new
+# Changelog: https://github.com/<user>/grub-theme-polkadots-dark/blob/master/CHANGES
+# Documentation: https://github.com/<user>/grub-theme-polkadots-dark/wiki
+# Repository-Browse: https://github.com/<user>/grub-theme-polkadots-dark
+# Repository: https://github.com/<user>/grub-theme-polkadots-dark.git
